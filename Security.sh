288317e2d88e7b3bf670393c6db77fcb0269a169
Your Name <you@example.com>
2024-10-13 01:46:45 -0300

    Encrypted DNS Proxy

--- Security.sh
+++ Security.sh
@@ -7,7 +7,9 @@
 
     echo "MastersUniverse" > /etc/hostname
     echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-
+    echo "nameserver 127.0.0.1" >> /etc/resolv.conf
+    echo "nameserver 127.0.2.1" >> /etc/resolv.conf
+    
     printf " ${GREEN}[+] Clean Old Versions\n"${NC}
     sudo rm /etc/systemd/system/macchange.service 
     sudo rm /etc/systemd/system/multi-user.target.wants/dnscrypt-proxy*
@@ -41,6 +43,12 @@
     
     printf " ${GREEN}[+] Downloading ADBlockers...\n"${NC}
     wget https://gist.githubusercontent.com/GameOverStudios/8e2a16dbf62902a8094943c5d0c8dc82/raw/update-adblocker.sh -O /etc/dnscrypt-proxy/update-adblocker.sh
+    echo ""
+
+    printf " ${GREEN}[+] Downloading Encrypted DNS Server...\n"${NC}
+    wget https://github.com/DNSCrypt/encrypted-dns-server/releases/download/0.9.16/encrypted-dns_0.9.16_amd64.deb
+    wget https://raw.githubusercontent.com/DNSCrypt/encrypted-dns-server/refs/heads/master/example-encrypted-dns.toml -O /etc/dnscrypt-proxy/encrypted-dns.toml
+    sudo dpkg -i encrypted-dns_0.9.16_amd64.deb
     echo ""
 
     tar -xzvf dnscrypt-proxy-linux_x86_64-2.1.5.tar.gz
@@ -152,27 +160,57 @@ EOF
         -e 's/block_ipv6 = false/block_ipv6 = true/' \
         -e 's/# blocked_names_file =/blocked_names_file =/' \
         -e 's/blocked-names.txt/blocked_names.txt/' \
+        -e 's/force_tcp = false/force_tcp = true/' \
         /etc/dnscrypt-proxy/dnscrypt-proxy.toml
     echo ""
 
+    sudo sed -i \
+        -e 's/9.9.9.9:53/8.8.8.8:53/' \
+        /etc/dnscrypt-proxy/encrypted-dns.toml
+
     printf " ${GREEN}[+] Starting DNSCrypt....\n"${NC}
     sudo ./dnscrypt-proxy -service install
-    sudo systemctl enable dnscrypt-proxy
-    sudo systemctl start dnscrypt-proxy
-    sudo systemctl status dnscrypt-proxy
+    #sudo systemctl enable dnscrypt-proxy
+    #sudo systemctl start dnscrypt-proxy
+    #sudo systemctl status dnscrypt-proxy
 q
     printf " ${GREEN}[+] Verify DNS Service\n"${NC}
     ./dnscrypt-proxy -config /etc/dnscrypt-proxy/dnscrypt-proxy.toml -resolve google.com
     nmtui
-    
-    nohup /etc/macchange &
-    nohup /etc/dnscrypt-proxy/dnscrypt-proxy &
 
+    printf " ${GREEN}[+] Tor Service\n"${NC}
     sudo apt install tor torsocks -y
     sudo syatemctl enable tor
     sudo systemctl start tor
     sudo systemctl status tor
 
+    printf " ${GREEN}[+] Create Executable\n"${NC}
+    echo 'echo "MastersUniverse" > /etc/hostname' >> /etc/dnscrypt-proxy/start.sh
+    echo 'echo "nameserver 8.8.8.8" >> /etc/resolv.conf' >> /etc/dnscrypt-proxy/start.sh
+    echo 'echo "nameserver 127.0.0.1" >> /etc/resolv.conf' >> /etc/dnscrypt-proxy/start.sh
+    echo 'echo "nameserver 127.0.2.1" >> /etc/resolv.conf' >> /etc/dnscrypt-proxy/start.sh
+    echo "nohup /etc/macchange &" >> /etc/dnscrypt-proxy/start.sh
+    echo "nohup /etc/dnscrypt-proxy/dnscrypt-proxy &" >> /etc/dnscrypt-proxy/start.sh
+    echo "nohup encrypted-dns -c /etc/dnscrypt-proxy/encrypted-dns.toml &" >> /etc/dnscrypt-proxy/start.sh
+    chmod +x /etc/dnscrypt-proxy/start.sh
+    
+    printf " ${GREEN}[+] DNS Stamp\n"${NC}
+    encrypted-dns -c /etc/dnscrypt-proxy/encrypted-dns.toml
+    echo "DNS Stamp:"
+    read dns_stamp
+
+    -e 's/# [static.myserver]/[static.myserver]/' \
+        -e '/stamp = 'sdns://AQcAAAAAAAAAAAAQMi5kbnNjcnlwdC1jZXJ0Lg/stamp = '$stamp/' \
+        /etc/dnscrypt-proxy/dnscrypt-proxy.toml
+    
     echo ""
-    printf " ${GREEN}[+] Lembre-se de tirar 8.8.8.8 do firewall depois do dnscrypt baixar os repositorios a primeira vez\n"${NC}
+    printf " ${GREEN}[+] \nOpenSnitch
+                + 127.0.0.1:53 (udp) \n
+                google-chrome 80, 443
+                dnscrypt-proxy 9050, 443 (tcp, udp)
+                retirar 8.8.8.8 firewall
+    "${NC}
     echo ""
+
+    printf " ${GREEN}[+] Start\n"${NC}
+    /etc/dnscrypt-proxy/./start
